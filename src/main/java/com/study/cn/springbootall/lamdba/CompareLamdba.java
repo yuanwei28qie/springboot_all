d5afb91e56e0b313a6f1372f57c2340a64c0858a
yuanw <yuanwei28qie@163.com>
2019-09-09 13:24:11 +0800

    rabbitMQ-demo

---
+++ src/main/java/com/study/cn/springbootall/lamdba/CompareLamdba.java
@@ -0,0 +1,14 @@
+package com.study.cn.springbootall.lamdba;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author huwei
+ * @date 2019/8/29 15:50
+ * <p> description:</>
+ */
+public class CompareLamdba {
+
+   
+}
---
+++ src/main/java/com/study/cn/springbootall/rabbitmq/DemoRabbitMQ.java
@@ -0,0 +1,23 @@
+package com.study.cn.springbootall.rabbitmq;
+
+/**
+ * @author huwei
+ * @date 2019/9/4 16:39
+ * <p> description:rabbitMQ 基于AMQP协议（高级消息队列协议），是erlang语言开发，
+ * 开源的消息代理和队列服务器
+ * 作用：通过普通协议在不同的应用服务器中共享数据；跨语言和跨平台使用
+ * 优点：
+ *      开源。
+ *      服务器与服务器之间的链接消耗性能低。
+ *      集群模式丰富。
+ *      集成springAMQP。
+ *
+ *AMQP协议模型；
+ *      producor（生产者）-> server(服务端)-> virtual host（虚拟主机）
+ *      ->exchange（交换机）->queue（队列）->customer(消费者)
+ *
+ *
+ * </>
+ */
+public class DemoRabbitMQ {
+}
